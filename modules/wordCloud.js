0aa40ef165daa951e810bf28c14f3f820f7b86f1
cesine <cesine@yahoo.com>
2015-09-11 20:18:45 -0400

    when text changes recalculate word frequencies

--- modules/wordCloud.js
+++ modules/wordCloud.js
@@ -8,6 +8,44 @@
 // By Curran Kelleher August 2014
 define(["d3", "d3_cloud", "lodash", "model", "modelContrib/reactivis"], function(d3, cloud, _, Model, Reactivis) {
 
+
+  // Remove punctation and count repeated words. 
+  // Kinda works on languages that have spaces
+  var getRoughWordCountsFromAText = function(text) {
+    var frequencyMap = {
+      tokens: 0,
+      types: 0
+    };
+    text
+      .replace(/[!\.,:;\?&+_-]/g, ' ')
+      .split(/[ \t\n]+/)
+      .map(function(word) {
+        if (!word || word.length < 3) {
+          return;
+        }
+        if (frequencyMap[word]) {
+          frequencyMap[word].count++;
+        } else {
+          frequencyMap[word] = {
+            text: word,
+            count: 1
+          };
+          frequencyMap.types++;
+        }
+        frequencyMap.tokens++;
+      });
+
+    var wordsArray = [];
+    for (var word in frequencyMap) {
+      if (frequencyMap.hasOwnProperty(word) && word !== "tokens" && word !== "types") {
+        frequencyMap[word].importance = frequencyMap[word].count / frequencyMap.types;
+        wordsArray.push(frequencyMap[word]);
+      }
+    }
+    wordsArray.tokenCount = frequencyMap.tokens;
+    return wordsArray;
+  };
+
   // The module constructor accepts a `container` argument,
   // which is an empty DOM element such as a div.
   return function WordCloud(container) {
@@ -34,16 +72,12 @@ define(["d3", "d3_cloud", "lodash", "model", "modelContrib/reactivis"], function
     // Use conventional margins.
     Reactivis.margin(model);
 
-    // Use an ordinal X axis for defining bars.
-    Reactivis.xOrdinalScale(model);
-    Reactivis.xAxis(model);
-
-    // Use a Y linear axis with zero as the minimum for bar height.
-    Reactivis.yLinearScale(model);
-    Reactivis.yDomain(model, {
-      zeroMin: true
-    });
-    Reactivis.yAxis(model);
+    model.when(["text"], _.throttle(function(text) {
+      model.data = getRoughWordCountsFromAText(text.replace(/[0-9]/g, ''));
+      model.size = model.data.tokenCount;
+      delete model.data.tokenCount;
+      console.log(" Data is now" + model.size, model.data);
+    }), transitionDuration);
 
     // Draw the bars.
     model.when(["g", "data", "xAttribute", "yAttribute", "xScale", "yScale", "height"],
